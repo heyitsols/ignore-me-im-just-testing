75da88f441747cd9466d6405e86403bf424fab14
Oliver Leaver-Smith <oliver.leaver-smith@skybettingandgaming.com>
2017-07-18 16:49:07 +0100

    move stuff around

--- index.php
+++ index.php
@@ -4,9 +4,9 @@ $host = $_SERVER['HTTP_HOST'];
 
 switch ($host) {
 case "heroku.leaversmith.com":
-    include('../site/index.php');
+    include('site/index.php');
     break;
 case "bins.leaversmith.com":
-    include('../bins/index.php');
+    include('bins/index.php');
     break;
 }
